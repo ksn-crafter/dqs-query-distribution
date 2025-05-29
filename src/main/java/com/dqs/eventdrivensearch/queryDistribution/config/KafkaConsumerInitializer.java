d5288fe952ce29a00b5220e9ad13ba651e35d4f7
suhrud-caizin <suhrud@caizin.com>
2025-05-29 12:33:01 +0530

    reads tenants for dynamic consumers from application.yaml

--- src/main/java/com/dqs/eventdrivensearch/queryDistribution/config/KafkaConsumerInitializer.java
+++ src/main/java/com/dqs/eventdrivensearch/queryDistribution/config/KafkaConsumerInitializer.java
@@ -2,7 +2,10 @@ package com.dqs.eventdrivensearch.queryDistribution.config;
 
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 import java.util.List;
 
 @Component
@@ -11,10 +14,15 @@ public class KafkaConsumerInitializer {
     @Autowired
     private DynamicKafkaConsumerConfiguration dynamicKafkaConsumerConfiguration;
 
+    @Value("${tenants}")
+    private String tenants;
+
     @PostConstruct
     public void initializeListeners() {
-        List<String> tenantIds = List.of("jpmc", "deutsche");
-        for (String suffix : tenantIds) {
+
+        List<String> tenants = Arrays.stream(this.tenants.split(",")).map(String::trim).map(String::toLowerCase).toList();
+
+        for (String suffix : tenants) {
             dynamicKafkaConsumerConfiguration.registerConsumerForTopic(suffix);
         }
     }
