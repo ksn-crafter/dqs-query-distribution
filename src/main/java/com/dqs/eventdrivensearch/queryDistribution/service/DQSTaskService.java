5e66ef5c9fab590edfbf1bccc17742e5816712f9
vinayak-caizin <vinayak@caizin.com>
2025-08-26 13:29:10 +0530

    Added Transactional annotation to DQSTaskService

--- src/main/java/com/dqs/eventdrivensearch/queryDistribution/service/DQSTaskService.java
+++ src/main/java/com/dqs/eventdrivensearch/queryDistribution/service/DQSTaskService.java
@@ -8,6 +8,7 @@ import com.dqs.eventdrivensearch.queryDistribution.model.DQSTask;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -23,8 +24,7 @@ public class DQSTaskService {
     @Value("${worker_count_for_dqs_task}")
     private int numberOfWorkers;
 
-
-
+    @Transactional
     public void generateDQSTask(QueryDescription queryDescription) {
 
         List<DQSSplitMetadata> splitMetadataList = splitMetadataRepository.findByTenantId(queryDescription.tenantId());
